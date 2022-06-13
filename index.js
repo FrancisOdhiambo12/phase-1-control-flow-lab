cbe77a5abcc92d56a81eb9f2a9ddb7f3ad435a5d
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-13 10:45:53 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+ let ride
+ if (someValue <= 400) {
+  ride = "This one is on me!"
+ } else if (someValue <= 2500){
+  ride = "I will gladly take your thirty bucks."
+ } else {
+  ride = "No can do."
+ }
+ return ride
+}
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  let message
+  if (city === "NYC") {
+    message = "Ok, sounds good."
+  }else {
+    message = "No go."
+  }
+  return message
+}
+ternaryCheckCity("NYC")
+ternaryCheckCity("Pittsburgh")
+
+function switchOnCharmFromTip(tip){
+  // Write your code here! 
+
+
+switch (tip) {
+  case "generous": 
+    return "Thank you so much.";
+    break;
+  
+  case "not as generous": 
+  return "Thank you.";
+    break;
+  
+  default: 
+    return "Bye.";
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// switch (description) {
+//  case "not as generous":
+//   return "Thank you so much."
+//   break;
+//   case "thanks for everything":
+//     return "Bye."
+//     break;
+//     default:
+//       return "Thank you so much."
+  
+// }
+
+  // let text 
+  // if (tip == "generous") {
+  //   text = "Thank you so much."
+  // } else if (tip !== " generous") {
+  //   text = "Thank you."
+  // } else {
+  //   text = "Bye."
+  // }
+  // return text
+  
+// control flow done
